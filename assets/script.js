bbc3293bab589723e46f3cd8e3d7a3e1ff1bda32
Anam Ahmed <anam.ahmed.a@gmail.com>
2015-07-08 12:52:39 +0600

    missing false like values fixed, array problems fixed

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,40 @@
 var repl = document.querySelector('.repl');
-var fakeConsole = {
-    log:function(output){
-        var line = document.createElement('li');
-        line.innerHTML = output;
-        document.querySelector('.result .lines').appendChild(line);
-        console.log(output);
+var fakeConsole = Object.create(console);
+
+fakeConsole.log = function(){
+    // Handles multiple arguments for console
+    // handle false like values
+    var outputArray = [];
+    for(i=0;i<arguments.length;i++){
+        var currentElement = arguments[i];
+        // if not NaN
+        if(currentElement==currentElement){
+            if(currentElement===null){
+                outputArray.push("null");
+            }
+            else if(currentElement===undefined){
+                outputArray.push("undefined");
+            }
+            else if(Array.isArray(currentElement)){ 
+                //handle arrays including empty ones
+                var arrayBody = "["+currentElement.join(",")+"]";
+                outputArray.push(arrayBody);
+            }
+            else{
+                outputArray.push(currentElement);
+            }
+        }
+        else{
+            outputArray.push("NaN");
+        }
     }
+
+    var line = document.createElement('li'),
+        output = Array.prototype.join.call(outputArray, ", ");
+
+    line.innerHTML = output;
+    document.querySelector('.result .lines').appendChild(line);
+    console.log(arguments);
 };
 var replManager = CodeMirror(repl,{
     mode:'javascript',
@@ -23,8 +52,8 @@ function evaluate(){
     //clear output if any
     document.querySelector('.result .lines').innerHTML="";
     //load the contents as a function
-   var expressions = replManager.getValue();
-    var action = new Function('console',expressions);
+    var expressions = replManager.getValue();
+    var action = new Function('console', expressions);
     action(fakeConsole);
     window.scrollTo(0, document.body.scrollHeight);
 };
