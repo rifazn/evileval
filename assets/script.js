54720e34fc54f49c17ae2217b79bc1cf5ae65693
Anam Ahmed <anam.ahmed.a@gmail.com>
2015-07-08 22:10:54 +0600

    continuous mode added in the live version

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,159 @@
 var repl = document.querySelector('.repl');
-var fakeConsole = {
-    log:function(output){
-        var line = document.createElement('li');
-        line.innerHTML = output;
-        document.querySelector('.result .lines').appendChild(line);
-        console.log(output);
+var fakeConsole = Object.create(console);
+
+fakeConsole.log = function(){
+    // Handles multiple arguments for console
+    // handle false like values
+    var outputArray = [];
+    for(i=0;i<arguments.length;i++){
+        var currentElement = arguments[i];
+        // if not NaN
+        if(currentElement==currentElement){
+            if(currentElement===null){
+                outputArray.push("null");
+            }
+            else if(currentElement===undefined){
+                outputArray.push("undefined");
+            }
+            else if(Array.isArray(currentElement)){
+                //handle arrays including empty ones
+                var arrayBody = "["+currentElement.join(",")+"]";
+                outputArray.push(arrayBody);
+            }
+            else{
+                outputArray.push(currentElement);
+            }
+        }
+        else{
+            outputArray.push("NaN");
+        }
     }
+
+    var line = document.createElement('li'),
+        output = Array.prototype.join.call(outputArray, ", ");
+
+    line.innerHTML = output;
+    document.querySelector('.result .lines').appendChild(line);
+    console.log(arguments);
 };
 var replManager = CodeMirror(repl,{
     mode:'javascript',
     theme:'monokai',
     lineNumbers:true
 });
-
-document.querySelector('.action').addEventListener('click',evaluate);
-document.addEventListener('keyup',function(e){
-    if(e.ctrlKey && e.key=='e'){
-        evaluate();
+replManager.on('change',function() {
+    if(isContinuous) {
+        evaluateDebounced()
     }
 })
+
+document.querySelector('.action').addEventListener('click',function(e){
+    if(e.shiftKey || isContinuous) {
+        toggleContinuous();
+    } else {
+        evaluate();
+    }
+});
+document.addEventListener('keyup',function(e){
+    if(e.ctrlKey && e.key=='e'){
+        if(e.shiftKey || isContinuous) {
+            toggleContinuous();
+        } else {
+            evaluate();
+        }
+    }
+})
+
+var isContinuous = false;
+function toggleContinuous(){
+    isContinuous = !isContinuous;
+    document.querySelector('.action').classList.toggle('active', isContinuous);
+}
+
+var worker;
 function evaluate(){
+    if(worker) {
+        worker.terminate()
+        worker = null
+    }
+    worker = new Worker(window.URL.createObjectURL(new Blob([
+        "var output = [];\n"+
+        "\n"+
+        "var fakeConsole = {\n"+
+        "    log: function() {\n"+
+        "        postMessage({\n"+
+        "            kind: 'call',\n"+
+        "            func: 'console.log',\n"+
+        "            args: Array.prototype.slice.call(arguments)\n"+
+        "        });\n"+
+        "    }\n"+
+        "};\n"+
+        "\n"+
+        "onmessage = function(e){\n"+
+        "    var action = new Function('console', e.data.expressions);\n"+
+        "    action(fakeConsole);\n"+
+        "    \n"+
+        "    postMessage({\n"+
+        "        kind: 'exit'\n"+
+        "    });\n"+
+        "}\n"
+    ], {
+      type: 'text/javascript'
+    })));
+
     //clear output if any
     document.querySelector('.result .lines').innerHTML="";
-    //load the contents as a function
-   var expressions = replManager.getValue();
-    var action = new Function('console',expressions);
-    action(fakeConsole);
+
+    var length = 0;
+
+    var timeout = setTimeout(function() {
+        worker.terminate();
+        fakeConsole.log('[Terminated! Script took too long to end..]');
+    }, 1000);
+    worker.onmessage = function(e) {
+        switch(e.data.kind) {
+            case 'exit':
+                clearTimeout(timeout);
+                break
+
+            case 'call':
+                switch(e.data.func) {
+                    case 'console.log':
+                        length++;
+                        fakeConsole.log.apply(fakeConsole, e.data.args);
+
+                        if(length > 1000) {
+                            clearTimeout(timeout);
+                            worker.terminate();
+                            fakeConsole.log('[Terminated! Script produced too much output..]');
+                        }
+                }
+                break
+        }
+    }
+    worker.onerror = function(e) {
+        e.preventDefault()
+        clearTimeout(timeout);
+        worker.terminate();
+        fakeConsole.log(e.message + ' (at line '+(e.lineno-2)+')')
+    }
+    worker.postMessage({
+        expressions: replManager.getValue()
+    })
+
     window.scrollTo(0, document.body.scrollHeight);
 };
+
+// Taken from https://remysharp.com/2010/07/21/throttling-function-calls
+function debounce(fn, delay){
+    var timer = null;
+    return function () {
+        var context = this, args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            fn.apply(context, args);
+        }, delay);
+    };
+}
+
+var evaluateDebounced = debounce(evaluate, 1000);
